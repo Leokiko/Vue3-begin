bd121f6fc270255db61e741d9f651085240ef180
Leokiko <o2239@qq.com>
2022-06-15 16:22:22 +0800

    change-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+
+import first from '@/components/first'
+import second from "@/components/second"
+import third from "@/components/third"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        // {
+        //         path: '/',
+        //         name: 'HelloWorld',
+        //         component: HelloWorld
+        //     },
+        {
+            path: '/',
+            name: 'first',
+            component: first
+        }, {
+            path: '/second',
+            name: 'second',
+            component: second
+        }, {
+            path: '/third',
+            name: 'third',
+            component: third
+        }
+    ]
 })
